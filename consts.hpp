8c9345cca4b0e1d60637e1b5a14e2ae1d93251e8
narekchj <n.chukhajyan@gmail.com>
2021-10-16 00:29:58 +0400

    consts

---
+++ consts.hpp
@@ -0,0 +1,11 @@
+#ifndef CONSTS
+#define CONSTS
+
+inline constexpr std::size_t operator "" _sz(unsigned long long val) { return val; }
+
+inline const auto PI = 3.14f;
+inline const auto myu_0 = 4 * PI * 1E-7;
+inline const auto Ro20 = 0.0175f;
+inline const auto max_contures = 1000_sz;
+
+#endif //CONSTS
